3a455017e11034b860f6e580922478b1edd9f73b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-24 17:28:47 +0530

    sign in functionality completed

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,12 @@
-import { Link } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Link, router } from "expo-router";
 import { useState } from "react";
+import { Alert, Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import CustomButton from "../../components/custom-button";
 import FormField from "../../components/form-field";
 import { images } from "../../constants";
+import { signIn } from "../../lib/appwrite";
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -14,7 +15,27 @@ const SignIn = () => {
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const onSubmit = () => {};
+  const onSubmit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all the fields.");
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const result = await signIn(form.email, form.password);
+
+      // set it to the global state
+
+      router.replace("/home");
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error", error?.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
