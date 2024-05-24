dd2f4542d14253a6609eb5b811dcce24536da121
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-24 11:34:30 +0530

    poppins font setup

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora</Text>
+      <Text className="text-3xl font-pblack">Aora</Text>
       <StatusBar style="auto" />
       <Link href="/profile" className="text-blue-500">
         Go to profile
