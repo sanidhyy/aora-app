752e50a4038667e4a6eb603d113653022300089c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-25 14:21:29 +0530

    create post functionality completed

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,13 +1,25 @@
 import { ResizeMode, Video } from "expo-av";
+import * as ImagePicker from "expo-image-picker";
+import { router } from "expo-router";
 import { useState } from "react";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import CustomButton from "../../components/custom-button";
 import FormField from "../../components/form-field";
 import { icons } from "../../constants";
-import CustomButton from "../../components/custom-button";
+import { useGlobalContext } from "../../context/global-provider";
+import { createVideo } from "../../lib/appwrite";
 
 const Create = () => {
+  const { user } = useGlobalContext();
   const [isUploading, setIsUploading] = useState(false);
   const [form, setForm] = useState({
     title: "",
@@ -17,10 +29,54 @@ const Create = () => {
   });
 
   const openPicker = async (selectType) => {
-    const result = await DocumentPicker;
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:
+        selectType === "image"
+          ? ImagePicker.MediaTypeOptions.Images
+          : ImagePicker.MediaTypeOptions.Videos,
+      aspect: [4, 3],
+      quality: 1,
+      videoQuality: 1,
+    });
+
+    if (result.canceled) return;
+
+    if (selectType === "image") {
+      setForm({ ...form, thumbnail: result.assets[0] });
+    }
+
+    if (selectType === "video") {
+      setForm({ ...form, video: result.assets[0] });
+    }
   };
 
-  const onSubmit = () => {};
+  const onSubmit = async () => {
+    if (!form.prompt || !form.title || !form.video || !form.thumbnail) {
+      return Alert.alert("Error", "Please fill in all the fields.");
+    }
+
+    setIsUploading(true);
+
+    try {
+      await createVideo({
+        ...form,
+        userId: user.$id,
+      });
+
+      Alert.alert("Success", "Post uploaded successfuly!");
+      router.push("/home");
+    } catch (error) {
+      Alert.alert("Error", error?.message);
+    } finally {
+      setForm({
+        title: "",
+        prompt: "",
+        video: null,
+        thumbnail: null,
+      });
+      setIsUploading(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -41,14 +97,16 @@ const Create = () => {
             Upload video
           </Text>
 
-          <TouchableOpacity onPress={() => openPicker("video")}>
+          <TouchableOpacity
+            onPress={() => openPicker("video")}
+            disabled={isUploading}
+            className={`${isUploading && "opacity-50"}`}
+          >
             {form.video ? (
               <Video
                 source={{ uri: form.video.uri }}
                 className="w-full h-64 rounded-2xl"
-                useNativeControls
                 resizeMode={ResizeMode.COVER}
-                isLooping
               />
             ) : (
               <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
@@ -69,7 +127,11 @@ const Create = () => {
             Thumbnail Image
           </Text>
 
-          <TouchableOpacity onPress={() => openPicker("image")}>
+          <TouchableOpacity
+            onPress={() => openPicker("image")}
+            disabled={isUploading}
+            className={`${isUploading && "opacity-50"}`}
+          >
             {form.thumbnail ? (
               <Image
                 source={{ uri: form.thumbnail.uri }}
@@ -95,7 +157,7 @@ const Create = () => {
         <FormField
           title="AI Prompt"
           value={form.prompt}
-          placeholder="The prompt you used to create this video"
+          placeholder="The prompt you used to create it"
           handleChangeText={(e) => setForm({ ...form, prompt: e })}
           otherStyles="mt-7"
           isLoading={isUploading}
