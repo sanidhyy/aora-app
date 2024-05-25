d0f6d60dd29813546bf087587449d00d61d33f9b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-25 13:39:18 +0530

    create screen ui implemented

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,10 +1,110 @@
-import { Text, View } from "react-native";
+import { ResizeMode, Video } from "expo-av";
+import { useState } from "react";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import FormField from "../../components/form-field";
+import { icons } from "../../constants";
+import CustomButton from "../../components/custom-button";
 
 const Create = () => {
+  const [isUploading, setIsUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    prompt: "",
+    video: null,
+    thumbnail: null,
+  });
+
+  const onSubmit = () => {};
+
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white text-psemibold">Upload Video</Text>
+
+        <FormField
+          title="Video title"
+          value={form.title}
+          placeholder="Give your video a title..."
+          handleChangeText={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-10"
+          isLoading={isUploading}
+        />
+
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Upload video
+          </Text>
+
+          <TouchableOpacity>
+            {form.video ? (
+              <Video
+                source={{ uri: form.video.uri }}
+                className="w-full h-64 rounded-2xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
+            ) : (
+              <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
+                <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
+                  <Image
+                    source={icons.upload}
+                    resizeMode="contain"
+                    className="w-1/2 h-1/2"
+                  />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 text-pmedium">
+            Thumbnail Image
+          </Text>
+
+          <TouchableOpacity>
+            {form.thumbnail ? (
+              <Image
+                source={{ uri: form.thumbnail.uri }}
+                resizeMode="cover"
+                className="w-full h-64 rounded-2xl"
+              />
+            ) : (
+              <View className="w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2">
+                <Image
+                  source={icons.upload}
+                  resizeMode="contain"
+                  className="w-5 h-5"
+                />
+
+                <Text className="text-sm text-gray-100 font-pmedium">
+                  Choose a file
+                </Text>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <FormField
+          title="AI Prompt"
+          value={form.prompt}
+          placeholder="The prompt you used to create this video"
+          handleChangeText={(e) => setForm({ ...form, prompt: e })}
+          otherStyles="mt-7"
+          isLoading={isUploading}
+        />
+
+        <CustomButton
+          title="Submit & Publish"
+          handlePress={onSubmit}
+          containerStyles="mt-7"
+          isLoading={isUploading}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- components/form-field.jsx
+++ components/form-field.jsx
@@ -19,7 +19,7 @@ const FormField = ({
 
       <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
         <TextInput
-          className="flex-1 text-white font-psemibold text-base"
+          className="flex-1 text-white font-psemibold text-xs"
           value={value}
           placeholder={placeholder}
           placeholderTextColor="#7b7b8b"
